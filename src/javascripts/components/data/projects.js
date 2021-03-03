94e84494284391aaf7394b3e7ebe694d5d9baca4
Chie Stroud <urirunrunkoko@gmail.com>
2021-03-03 12:50:36 -0600

    made projects objects

---
+++ src/javascripts/components/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    title: 'Product Cards',
+    screenshot: '/images/productcard.png',
+    description: 'My very first project in NSS. Static website only using HTML and CSS. Used flexbox to display cards',
+    technologiesUsed: 'HTML, CSS',
+    available: true,
+    url: 'https://cs-product-cards.netlify.app/',
+    githubUrl: 'https://github.com/chiestroud/product-cards',
+  },
+  {
+    title: 'Pet Adoption',
+    screenshot: '/images/petadoption.png',
+    description: 'My second project in NSS. Dynamically created list of available pet cards using JavaScript',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript',
+    available: true,
+    url: 'https://cs-pet-adoption.netlify.app',
+    githubUrl: 'https://github.com/chiestroud/pet-adoption',
+  },
+  {
+    title: 'Sorting Hat',
+    screenshot: 'https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg',
+    description: 'My third project in NSS. Learned how to sort by name/school and got a better understanding of how to use eventlistener.', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript',
+    available: true,
+    url: 'https://cs-sorting-hat.netlify.app',
+    githubUrl: 'https://github.com/chiestroud/sorting-hat',
+  }
+
+];
+
+export default projects;
