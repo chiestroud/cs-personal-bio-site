7907957a30988c158e5527d9586a4b4f714cc133
Chie Stroud <urirunrunkoko@gmail.com>
2021-03-06 15:27:20 -0600

    importing pictures

--- src/javascripts/components/data/projects.js
+++ src/javascripts/components/data/projects.js
@@ -1,51 +1,3 @@
-const projects = [
-  {
-    title: 'Product Cards',
-    screenshot: '/images/productcard.png',
-    description: 'My very first project in NSS. Static website only using HTML and CSS. Used flexbox to display cards',
-    technologiesUsed: 'HTML, CSS',
-    available: true,
-    url: 'https://cs-product-cards.netlify.app/',
-    githubUrl: 'https://github.com/chiestroud/product-cards',
-  },
-  {
-    title: 'Pet Adoption',
-    screenshot: '/images/petadoption.png',
-    description: 'My second project in NSS. Dynamically created list of available pet cards using JavaScript',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript',
-    available: true,
-    url: 'https://cs-pet-adoption.netlify.app',
-    githubUrl: 'https://github.com/chiestroud/pet-adoption',
-  },
-  {
-    title: 'Sorting Hat',
-    screenshot: '/images/sortinghat.png',
-    description: 'My third project in NSS. Learned how to sort by name/school and got a better understanding of how to use eventlistener.', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript',
-    available: true,
-    url: 'https://cs-sorting-hat.netlify.app',
-    githubUrl: 'https://github.com/chiestroud/sorting-hat',
-  },
-  {
-    title: 'Bear Watcher',
-    screenshot: '/images/bearwatcher.png',
-    description: 'My fourth project Bear Watcher',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript',
-    available: true,
-    url: 'https://cs-bear-watcher.netlify.app/',
-    githubUrl: 'https://github.com/chiestroud/ASSIGNMENT-Bear-Watcher',
-  },
-  {
-    title: 'Tamagotchi',
-    screenshot: '/../../../../images/tamagotchi.png',
-    description: 'My fifth project Tamagotchi',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript',
-    available: true,
-    url: 'https://cs-tamagotchi.netlify.app/',
-    githubUrl: 'https://github.com/chiestroud/ASSIGNMENT-Tamagotchi',
-  }
-];
-
 const projectBuilder = (array) => {
   document.querySelector('#welcome').innerHTML = '';
   document.querySelector('#technology').innerHTML = '';
@@ -66,4 +18,4 @@ const projectBuilder = (array) => {
   });
 };
 
-export { projects, projectBuilder };
+export default projectBuilder;
