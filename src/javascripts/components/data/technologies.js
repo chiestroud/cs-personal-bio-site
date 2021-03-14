930e9f6269fc8c7c23fad78f83bb6e790c4fa5d1
Chie Stroud <urirunrunkoko@gmail.com>
2021-03-14 18:26:03 -0500

    updated json files

--- src/javascripts/components/data/technologies.js
+++ src/javascripts/components/data/technologies.js
@@ -5,7 +5,7 @@ const technologyBuilder = () => {
   document.querySelector('#contacts').innerHTML = '';
   document.querySelector('#technology').innerHTML = `<div class="technology">
                                                           <img src="https://seeklogo.com/images/H/html5-without-wordmark-color-logo-14D252D878-seeklogo.com.png" alt="HTML">
-                                                          <img src="https://www.flaticon.com/svg/vstatic/svg/732/732190.svg?token=exp=1615071135~hmac=823f9493f05c1f2b13017f578f1f21e8" alt="CSS">
+                                                          <img src="https://seeklogo.com/images/C/css-3-logo-AF06D75231-seeklogo.com.png" alt="CSS">
                                                           <img src="https://seeklogo.com/images/J/javascript-js-logo-2949701702-seeklogo.com.png" alt="JS">
                                                           <img src="https://seeklogo.com/images/N/npm-node-package-manager-logo-DE93649ED1-seeklogo.com.png" alt="NPM">
                                                         </div>`;
